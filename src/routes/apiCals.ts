ae25faa35e20df787f357eb311ea20918ffec6e3
Matheus Dias <53883196+Diaszim13@users.noreply.github.com>
2024-12-18 07:40:06 -0300

    foi

--- src/routes/apiCals.ts
+++ src/routes/apiCals.ts
@@ -3,31 +3,33 @@ import axios from "axios";
 const router = express.Router();
 
 
-router.get("/api/", (request: Request, response: Response) => {
-  console.log(request.body);
+router.post("/api", (request: Request, response: Response) => {
   
-  axios.get(process.env.URL + "json/last/" + request.body.convert).then((res: any) => {
-    
+  axios.get(process.env.URL + "json/last/" + request.body).then((res: any) => {
+    console.log(res);
+	
     if (!res.data) response.status(300).render('error');
     
     response.status(200).render('result', {dados: res.data});
   })
+	
 });
 
 router.get("api/days:", (request: Request, response: Response) => {
 	try {
 			const params = request.body.data;
-			axios.get(process.env.URL + "json/daily:" + parmas.moeda + "/:" + params.dias).then(async (res: any) => {
-				if(!res.data) throw new Exception(response.getError());
+			axios.get(process.env.URL + "json/daily:" + params.moeda + "/:" + params.dias).then(async (res: any) => {
+				if (!res.data) response.status(300).render('error');
 				
 				response.status(200).send(res.data);
 		})
 	}
-	catch($e)
+	catch($e: any)
 	{
-		response.status(300).send($e.getError());
+		response.status(300).render('error');
 	}
 
-})
+});
+
 
 module.exports = router;
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 
 require('dotenv').config();
@@ -26,3 +27,80 @@ app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
+*/
+import * as grpc from '@grpc/grpc-js';
+import * as protoLoader from '@grpc/proto-loader';
+import axios from 'axios';
+import path from 'path';
+
+const PROTO_PATH = path.join(__dirname, './conversion.proto');
+
+// Carregando o proto com configurações explícitas
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+
+// Tipagem explícita do serviço
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition) as unknown as {
+  ConversionService: grpc.ServiceDefinition<any>;
+};
+
+const server = new grpc.Server();
+
+// Implementação dos métodos do serviço gRPC
+server.addService(protoDescriptor, {
+  GetLastConversion: (call: any, callback: any) => {
+    const convert = call.request.convert;
+
+    axios.get(`${process.env.URL}json/last/${convert}`)
+      .then((res: any) => {
+        if (!res.data) {
+          return callback({
+            code: grpc.status.NOT_FOUND,
+            details: 'Data not found',
+          });
+        }
+        callback(null, { data: JSON.stringify(res.data) });
+      })
+      .catch((err) => {
+        callback({
+          code: grpc.status.INTERNAL,
+          details: err.message,
+        });
+      });
+  },
+
+  GetDailyConversion: (call: any, callback: any) => {
+    const { moeda, dias } = call.request;
+
+    axios.get(`${process.env.URL}json/daily:${moeda}/:${dias}`)
+      .then((res: any) => {
+        if (!res.data) {
+          return callback({
+            code: grpc.status.NOT_FOUND,
+            details: 'Data not found',
+          });
+        }
+        callback(null, { data: JSON.stringify(res.data) });
+      })
+      .catch((err) => {
+        callback({
+          code: grpc.status.INTERNAL,
+          details: err.message,
+        });
+      });
+  },
+});
+
+// Iniciando o servidor gRPC
+server.bindAsync('127.0.0.1:50051', grpc.ServerCredentials.createInsecure(), (err, port) => {
+  if (err) {
+    console.error(`Error starting server: ${err.message}`);
+    return;
+  }
+  console.log(`Server running at http://127.0.0.1:${port}`);
+});
